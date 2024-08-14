8f6794fde6c62faf74eae602259eba93ff73a80c
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-08-14 10:33:01 -0300

    feat: Adicionar validações de CPF, valores e contas
    Adiciona validações de CPF, valores e contas na classe ValidadorUtils. Agora, é possível validar um número de CPF, verificar se um valor é positivo, se o saldo é suficiente para uma operação e se as contas de origem e destino são diferentes. Isso melhora a segurança e a integridade das operações bancárias.

--- src/com/banco/utils/ValidadorUtils.java
+++ src/com/banco/utils/ValidadorUtils.java
@@ -2,9 +2,20 @@ package com.banco.utils;
 
 import com.banco.dominio.conta.Conta;
 
+/**
+ * Classe utilitária para validações relacionadas a CPF, valores, e contas
+ * bancárias.
+ */
 public class ValidadorUtils {
+
+    /**
+     * Valida um número de CPF.
+     *
+     * @param cpf CPF a ser validado.
+     * @return True se o CPF for válido, false caso contrário.
+     */
     public boolean isCPF(String cpf) {
-        if (cpf == null || cpf.length() != 11 || cpf.matches(cpf.charAt(0) + "{11}")) {
+        if (cpf == null || cpf.length() != 11 || !cpf.matches("\\d{11}") || cpf.matches(cpf.charAt(0) + "{11}")) {
             return false;
         }
 
@@ -34,25 +45,47 @@ public class ValidadorUtils {
         }
     }
 
+    /**
+     * Valida se o valor é positivo.
+     *
+     * @param valor Valor a ser validado.
+     */
     public static void validarValor(double valor) {
         if (valor <= 0) {
             throw new IllegalArgumentException("O valor da operação deve ser positivo.");
         }
     }
 
+    /**
+     * Valida se o saldo é suficiente para uma operação.
+     *
+     * @param saldo Saldo da conta.
+     * @param valor Valor da operação.
+     */
     public static void validarSaldoSuficiente(double saldo, double valor) {
         if (saldo < valor) {
             throw new IllegalStateException("Saldo insuficiente para realizar a operação.");
         }
     }
 
+    /**
+     * Valida se a conta não é nula.
+     *
+     * @param conta Conta a ser validada.
+     */
     public static void validarContaNull(Conta conta) {
         if (conta == null) {
             throw new IllegalArgumentException("Conta não pode ser nula.");
         }
     }
 
-    public static void contasIguais(Conta contaOrigem, Conta contaDestino) {
+    /**
+     * Valida se as contas de origem e destino são diferentes.
+     *
+     * @param contaOrigem  Conta de origem.
+     * @param contaDestino Conta de destino.
+     */
+    public static void contasDiferentes(Conta contaOrigem, Conta contaDestino) {
         if (contaOrigem.equals(contaDestino)) {
             throw new IllegalArgumentException("As contas de origem e destino não podem ser iguais.");
         }
